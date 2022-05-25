a31760cc03b0f02363a1aa6aa5b0e5822153d64c
Alexis86Sevilla <79936098+Alexis86Sevilla@users.noreply.github.com>
2022-05-25 10:48:44 +0200

    footer designed

--- site/Kreitekfy/src/app/app.module.ts
+++ site/Kreitekfy/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ScreenOneComponent } from './home/screen-one/screen-one.component';
+import { ScreenTwoComponent } from './home/screen-two/screen-two.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ScreenOneComponent,
+    ScreenTwoComponent
   ],
   imports: [
     BrowserModule,
