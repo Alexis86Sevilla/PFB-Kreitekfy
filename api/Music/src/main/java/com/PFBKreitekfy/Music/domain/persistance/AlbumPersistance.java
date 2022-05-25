93fa66cdaa85bfe583091e47ea27831c99def132
Pablo Fornetti Martínez <pablofornetti@gmail.com>
2022-05-25 15:52:00 +0200

    add: endpoints

--- api/Music/src/main/java/com/PFBKreitekfy/Music/domain/persistance/AlbumPersistance.java
+++ api/Music/src/main/java/com/PFBKreitekfy/Music/domain/persistance/AlbumPersistance.java
@@ -1,4 +1,16 @@
 package com.PFBKreitekfy.Music.domain.persistance;
 
+import com.PFBKreitekfy.Music.domain.entity.Album;
+
+
+import java.util.List;
+import java.util.Optional;
+
 public interface AlbumPersistance {
+
+    List<Album> getAllAlbums();
+
+    Optional<Album> getAlbumById(Long albumId);
+
+    Album saveAlbum(Album album);
 }
--- api/Music/src/main/java/com/PFBKreitekfy/Music/domain/persistance/ArtistPersistance.java
+++ api/Music/src/main/java/com/PFBKreitekfy/Music/domain/persistance/ArtistPersistance.java
@@ -1,4 +1,16 @@
 package com.PFBKreitekfy.Music.domain.persistance;
 
+
+import com.PFBKreitekfy.Music.domain.entity.Artist;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ArtistPersistance {
+
+    List<Artist> getAllArtists();
+
+    Optional<Artist> getArtistById(Long artistId);
+
+    Artist saveArtist(Artist artist);
 }
