6928c6a4c11b09aa186477d1c011a82cd287f284
Pablo Sanz <pablosanzadeva@gmail.com>
2022-05-26 10:34:02 +0200

    add: endpoints

--- api/Music/src/main/java/com/PFBKreitekfy/Music/infraestructure/rest/ArtistRestController.java
+++ api/Music/src/main/java/com/PFBKreitekfy/Music/infraestructure/rest/ArtistRestController.java
@@ -1,4 +1,46 @@
 package com.PFBKreitekfy.Music.infraestructure.rest;
 
+
+import com.PFBKreitekfy.Music.application.dto.ArtistDTO;
+import com.PFBKreitekfy.Music.application.service.ArtistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class ArtistRestController {
+    private final ArtistService artistService;
+
+    @Autowired
+    public ArtistRestController(ArtistService artistService) {
+        this.artistService = artistService;
+    }
+
+    @CrossOrigin
+    @PostMapping(value = "/artists", produces = "application/json", consumes = "application/json")
+    ResponseEntity<ArtistDTO> insertArtist(@RequestBody ArtistDTO artistDTO) {
+        ArtistDTO artistSaved = this.artistService.saveArtist(artistDTO);
+        return new ResponseEntity<>(artistSaved, HttpStatus.CREATED);
+    }
+    @CrossOrigin
+    @GetMapping(value = "/artists", produces = "application/json")
+    ResponseEntity<List<ArtistDTO>> getAllIArtists() {
+        List<ArtistDTO> artist = this.artistService.getAllArtists();
+        return new ResponseEntity<>(artist, HttpStatus.OK);
+    }
+    @CrossOrigin
+    @GetMapping(value = "/artists/{artistId}")
+    ResponseEntity<ArtistDTO> getArtistById(@PathVariable Long albumId) {
+        Optional<ArtistDTO> artist = this.artistService.getArtistById(albumId);
+
+        if (artist.isPresent()) {
+            return new ResponseEntity<>(artist.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- api/Music/src/main/java/com/PFBKreitekfy/Music/infraestructure/rest/StyleRestController.java
+++ api/Music/src/main/java/com/PFBKreitekfy/Music/infraestructure/rest/StyleRestController.java
@@ -1,4 +1,46 @@
 package com.PFBKreitekfy.Music.infraestructure.rest;
 
+
+import com.PFBKreitekfy.Music.application.dto.StyleDTO;
+import com.PFBKreitekfy.Music.application.service.StyleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class StyleRestController {
+    private final StyleService styleService;
+
+    @Autowired
+    public StyleRestController(StyleService styleService) {
+        this.styleService = styleService;
+    }
+
+    @CrossOrigin
+    @PostMapping(value = "/styles", produces = "application/json", consumes = "application/json")
+    ResponseEntity<StyleDTO> insertStyle(@RequestBody StyleDTO styleDTO) {
+        StyleDTO styleSaved = this.styleService.saveStyle(styleDTO);
+        return new ResponseEntity<>(styleSaved, HttpStatus.CREATED);
+    }
+    @CrossOrigin
+    @GetMapping(value = "/styles", produces = "application/json")
+    ResponseEntity<List<StyleDTO>> getAllStyles() {
+        List<StyleDTO> styles = this.styleService.getAllStyles();
+        return new ResponseEntity<>(styles, HttpStatus.OK);
+    }
+    @CrossOrigin
+    @GetMapping(value = "/styles/{styleId}")
+    ResponseEntity<StyleDTO> getStyleById(@PathVariable Long styleId) {
+        Optional<StyleDTO> styles = this.styleService.getStyleById(styleId);
+
+        if (styles.isPresent()) {
+            return new ResponseEntity<>(styles.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
