2fadf9542ce691b55517c8a58db7e582eeebcdb0
Alexis86Sevilla <79936098+Alexis86Sevilla@users.noreply.github.com>
2022-05-31 16:58:18 +0200

    add: song details

--- site/Kreitekfy/src/app/entities/song-user/song-detail/song-detail.component.ts
+++ site/Kreitekfy/src/app/entities/song-user/song-detail/song-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Song } from '../../song/model/song.model';
+import { SongService } from '../../song/service/song.service';
 
 @Component({
   selector: 'app-song-detail',
@@ -7,17 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SongDetailComponent implements OnInit {
 
-  songId?:number;
+  songId?: number;
+  song!: Song;
 
-  constructor() { }
+  constructor(private songService: SongService, private router: ActivatedRoute) {
+
+  }
 
   ngOnInit(): void {
+    this.songId = +this.router.snapshot.paramMap.get("songId")!;
+    this.getSongById(this.songId);
   }
 
-  public getSongById(songId:number):void{
+  public getSongById(songId: number): void {
+    this.songService.getSongById(songId).subscribe({
+      next: (songRequest) => {
+        this.song = songRequest;
+      },
+      error: (err) => { this.handleError(err); }
+    })
 
 
-    
   }
 
+  private handleError(error: any): void {
+    console.log(error);
+  }
 }
