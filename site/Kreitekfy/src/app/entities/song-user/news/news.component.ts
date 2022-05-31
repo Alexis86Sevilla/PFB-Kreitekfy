9399ea095d92618ffa51c0d2be0a02c1da9475be
Alexis86Sevilla <79936098+Alexis86Sevilla@users.noreply.github.com>
2022-05-31 12:36:02 +0200

    add: new songs style and logic

--- site/Kreitekfy/src/app/entities/song-user/news/news.component.ts
+++ site/Kreitekfy/src/app/entities/song-user/news/news.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Pagination } from 'src/app/shared/pagination';
+import { Song } from '../../song/model/song.model';
+import { SongService } from '../service/song.service';
+
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss']
 })
-export class NewsComponent implements OnInit {
-
-  constructor() { }
+export class NewsComponent extends Pagination implements OnInit {
+  songs: Song[] = [];
+  constructor(private songUserService: SongService) {
+    super();
+  }
 
   ngOnInit(): void {
+    this.getNewSongs();
+  }
+
+  public getNewSongs(): void {
+    this.songUserService.getAllNewSongs()
+      .subscribe({
+        next: (data: any) => {
+          this.songs = data.content;
+        },
+        error: (err) => { this.handleError(err); }
+      })
+
+  }
+
+  private handleError(error: any): void {
+    console.log(error);
   }
 
 }
--- site/Kreitekfy/src/app/entities/song-user/service/song.service.ts
+++ site/Kreitekfy/src/app/entities/song-user/service/song.service.ts
@@ -7,10 +7,15 @@ import { Song } from '../../song/model/song.model';
   providedIn: 'root'
 })
 export class SongService {
-  //baseUri = "http://localhost:8081/kreitekfy/";
-  baseUri = "http://localhost:3003/";
+  baseUri = "http://localhost:8081/kreitekfy/";
+ // baseUri = "http://localhost:3003/";
 
   constructor(private http: HttpClient) { }
+
+  public getAllNewSongs(): Observable<Song[]> {
+    let urlEndpoint: string = "http://localhost:8081/kreitekfy/songs_filter?page=0&size=5&sort=dateLaunch,desc";
+    return this.http.get<Song[]>(urlEndpoint);
+  }
 
   public getSongsByStyle(styleId: number, page: number, size: number, sort: string): Observable<Song[]> {
     let urlEndpoint: string = this.baseUri + "songs/" + styleId + "?page=" + page + "&size=" + size + "&sort=" + sort;
